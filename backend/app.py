976a3316cfdf6cf964d321ec9c447e2750a65863
Yassin <138398718+sino2011@users.noreply.github.com>
2026-08-05 00:56:27 +0300

    Obscure DB_PASSWORD and DB_NAME in app.py
    Obscured sensitive database credentials in app.py.

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,8 @@ load_dotenv()
 
 DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_USER = os.getenv('DB_USER', 'root')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'Yassin2011')
-DB_NAME = os.getenv('DB_NAME', 'nexusfinancedashboard')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '###')
+DB_NAME = os.getenv('DB_NAME', '###')
 
 app = Flask(__name__)
 CORS(app, origins=["*"])
